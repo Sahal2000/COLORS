1963f3a67992ec86ebd7b6d12268b44b360387a6
md Sahal <me.sahal2000@gmail.com>
2021-06-26 22:28:20 +0600

    first commit

---
+++ colors.js
@@ -0,0 +1,114 @@
+function funCyan()
+{
+    document.getElementById('second').style.backgroundColor = 'cyan';
+}
+
+function funRed()
+{
+    document.getElementById('second').style.backgroundColor = 'red';
+}
+
+function funGreen()
+{
+    document.getElementById('second').style.backgroundColor = 'green';
+}
+
+function funOrange()
+{
+    document.getElementById('second').style.backgroundColor = 'green';
+}
+
+function funBlue()
+{
+    document.getElementById('second').style.backgroundColor = 'green';
+}
+
+function funBlack()
+{
+    document.getElementById('second').style.backgroundColor = 'green';
+}
+
+function funWhite()
+{
+    document.getElementById('second').style.backgroundColor = 'green';
+}
+
+function funViolet()
+{
+    document.getElementById('second').style.backgroundColor = 'green';
+}
+
+function funPink()
+{
+    document.getElementById('second').style.backgroundColor = 'green';
+}
+
+function funYellow()
+{
+    document.getElementById('second').style.backgroundColor = 'green';
+}
+
+function funBurlyWood()
+{
+    document.getElementById('second').style.backgroundColor = 'green';
+}
+
+function funChocolate()
+{
+    document.getElementById('second').style.backgroundColor = 'green';
+}
+
+function funIndigo()
+{
+    document.getElementById('second').style.backgroundColor = 'green';
+}
+
+function funSpringGreen()
+{
+    document.getElementById('second').style.backgroundColor = 'green';
+}
+
+function funTomato()
+{
+    document.getElementById('second').style.backgroundColor = 'green';
+}
+
+function funYellowGreen()
+{
+    document.getElementById('second').style.backgroundColor = 'green';
+}
+
+function funTurquoise()
+{
+    document.getElementById('second').style.backgroundColor = 'green';
+}
+
+function funTeal()
+{
+    document.getElementById('second').style.backgroundColor = 'green';
+}
+
+function funSteelBlue()
+{
+    document.getElementById('second').style.backgroundColor = 'green';
+}
+
+function funSeaGreen()
+{
+    document.getElementById('second').style.backgroundColor = 'green';
+}
+
+function funSalmon()
+{
+    document.getElementById('second').style.backgroundColor = 'green';
+}
+
+function funSandyBrown()
+{
+    document.getElementById('second').style.backgroundColor = 'green';
+}
+
+function funPurple()
+{
+    document.getElementById('second').style.backgroundColor = 'purple';
+}
