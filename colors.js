5809867513f78efd08e9e98001f4503399c31325
md Sahal <me.sahal2000@gmail.com>
2021-06-26 23:15:09 +0600

    second commit

--- colors.js
+++ colors.js
@@ -15,100 +15,105 @@ function funGreen()
 
 function funOrange()
 {
-    document.getElementById('second').style.backgroundColor = 'green';
+    document.getElementById('second').style.backgroundColor = 'orange';
 }
 
 function funBlue()
 {
-    document.getElementById('second').style.backgroundColor = 'green';
+    document.getElementById('second').style.backgroundColor = 'blue';
 }
 
 function funBlack()
 {
-    document.getElementById('second').style.backgroundColor = 'green';
+    document.getElementById('second').style.backgroundColor = 'black';
 }
 
 function funWhite()
 {
-    document.getElementById('second').style.backgroundColor = 'green';
+    document.getElementById('second').style.backgroundColor = 'white';
 }
 
 function funViolet()
 {
-    document.getElementById('second').style.backgroundColor = 'green';
+    document.getElementById('second').style.backgroundColor = 'violet';
 }
 
 function funPink()
 {
-    document.getElementById('second').style.backgroundColor = 'green';
+    document.getElementById('second').style.backgroundColor = 'pink';
 }
 
 function funYellow()
 {
-    document.getElementById('second').style.backgroundColor = 'green';
+    document.getElementById('second').style.backgroundColor = 'yellow';
 }
 
 function funBurlyWood()
 {
-    document.getElementById('second').style.backgroundColor = 'green';
+    document.getElementById('second').style.backgroundColor = 'burlywood';
 }
 
 function funChocolate()
 {
-    document.getElementById('second').style.backgroundColor = 'green';
+    document.getElementById('second').style.backgroundColor = 'chocolate';
 }
 
 function funIndigo()
 {
-    document.getElementById('second').style.backgroundColor = 'green';
+    document.getElementById('second').style.backgroundColor = 'indigo';
 }
 
 function funSpringGreen()
 {
-    document.getElementById('second').style.backgroundColor = 'green';
+    document.getElementById('second').style.backgroundColor = 'springgreen';
 }
 
 function funTomato()
 {
-    document.getElementById('second').style.backgroundColor = 'green';
+    document.getElementById('second').style.backgroundColor = 'tomato';
 }
 
 function funYellowGreen()
 {
-    document.getElementById('second').style.backgroundColor = 'green';
+    document.getElementById('second').style.backgroundColor = 'yellowgreen';
 }
 
 function funTurquoise()
 {
-    document.getElementById('second').style.backgroundColor = 'green';
+    document.getElementById('second').style.backgroundColor = 'turquoise';
 }
 
 function funTeal()
 {
-    document.getElementById('second').style.backgroundColor = 'green';
+    document.getElementById('second').style.backgroundColor = 'teal';
 }
 
 function funSteelBlue()
 {
-    document.getElementById('second').style.backgroundColor = 'green';
+    document.getElementById('second').style.backgroundColor = 'steelblue';
 }
 
 function funSeaGreen()
 {
-    document.getElementById('second').style.backgroundColor = 'green';
+    document.getElementById('second').style.backgroundColor = 'seaGreen';
 }
 
 function funSalmon()
 {
-    document.getElementById('second').style.backgroundColor = 'green';
+    document.getElementById('second').style.backgroundColor = 'salmon';
 }
 
 function funSandyBrown()
 {
-    document.getElementById('second').style.backgroundColor = 'green';
+    document.getElementById('second').style.backgroundColor = 'sandybrown';
 }
 
 function funPurple()
 {
     document.getElementById('second').style.backgroundColor = 'purple';
+}
+
+function funDarkcyan()
+{
+    document.getElementById('second').style.backgroundColor = 'darkcyan';
 }
